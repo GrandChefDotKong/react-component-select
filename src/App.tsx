720e9da21a877882fe6d1265c2304c706d8fb655
Adrien Pauchet <grandchefdotkong@gmail.com>
2023-01-20 18:40:01 +0900

    single select added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import { useState } from 'react'
-import logo from './logo.svg'
+import { useState } from 'react';
+import logo from './logo.svg';
+import Select from './Select';
+
+const options=[
+  { label: "First", value: 1 },
+  { label: "Second", value: 2 },
+  { label: "Thrid", value: 3 },
+  { label: "Fourth", value: 4 },
+
+]
 
 function App() {
+  const [value, setValue] = useState<typeof options[0] | undefined>(options[0]);
+
   return (
     <div className="App">
-      Hi
+      <Select options={options} value={value} onChange={o => setValue(o)} />
     </div>
   )
 }
